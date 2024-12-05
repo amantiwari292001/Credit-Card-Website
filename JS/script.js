9cb015fd8b16f3f081c064fab861df754c2ecac5
AMAN <amantiwari2912@gmail.com>
2024-12-05 22:39:41 +0530

    Day 4-> Header Completed

--- JS/script.js
+++ JS/script.js
@@ -1,2 +1,20 @@
 let header = document.querySelector('.header');
 let navbar = document.querySelector('.header .flex .navbar');
+let menuBtn = document.querySelector('#menu-btn');
+
+menuBtn.onclick = () =>{
+    menuBtn.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    menuBtn.classList.remove('fa-xmark');
+    navbar.classList.remove('active');
+    
+    if(window.scrollY > 0){
+        navbar.classList.add('active');
+    }else{
+        navbar.classList.remove('active');
+
+    }
+}
